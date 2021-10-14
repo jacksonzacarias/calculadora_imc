c47a0a8a60a06c0b8d3dd8310ab18148492bb10d
jackzacarias <goldenhorsescountry@gmail.com>
2021-10-14 01:12:49 -0300

    criação de botao e scroll

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
 
-
-
-void main (){
+void main() {
   runApp(MaterialApp(
     home: Home(),
   ));
 }
 
 class Home extends StatefulWidget {
-
-
   @override
   _HomeState createState() => _HomeState();
 }
@@ -20,39 +16,61 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Calculadora de IMC"),
+        title:const Text("Calculadora de IMC"),
         centerTitle: true,
         backgroundColor: Colors.green,
-        actions: <Widget> [
-          IconButton(icon: Icon(Icons.refresh),
-            onPressed: () {}, )
-        ],
-      ),
-      backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: const <Widget>[
-          Icon(Icons.person_outline, size: 120.0, color: Colors.green),
-          TextField(keyboardType: TextInputType.number,
-          decoration:  InputDecoration(
-            labelText: "Peso (kg)",
-            labelStyle: TextStyle(fontSize: 15.0, color: Colors.green)
-          ),
-          textAlign: TextAlign.center,
-          style: TextStyle(color: Colors.green,
-          ),
-          ),
-          TextField(keyboardType: TextInputType.number,
-          decoration:  InputDecoration(
-            labelText: "Altura (CM)",
-            labelStyle: TextStyle(fontSize: 15.0, color: Colors.green)
-          ),
-          textAlign: TextAlign.center,
-          style: TextStyle(color: Colors.green,
-          ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {},
           )
         ],
       ),
-    );
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children:  <Widget>[
+          Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Peso (kg)",
+                labelStyle: TextStyle(fontSize: 15.0, color: Colors.green)),
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.green,
+            ),
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Altura (CM)",
+                labelStyle: TextStyle(fontSize: 15.0, color: Colors.green)),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green),
+          ),
+          Padding(padding: EdgeInsets.only(top: 20.0, bottom: 10.0),
+            child: Container(
+            height: 50.0,
+            //width: 70.0,
+            child: RaisedButton(
+              onPressed: () {},
+              child: const Text(
+                "Calcular",
+                style: TextStyle(color: Colors.white, fontSize: 25.0),
+              ),
+              color: Colors.green,
+            ),
+          ),
+          ),
+          const Text("Info",
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.green, fontSize: 25.0),)
+        ],
+      ),
+      )  
+      );
   }
 }
