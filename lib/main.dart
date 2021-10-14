1b395f45a7e46b39fac46ce656666e85f9388bc6
jackzacarias <goldenhorsescountry@gmail.com>
2021-10-13 23:55:32 -0300

    Iniciando o projeto

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+
+
+void main (){
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Calculadora de IMC"),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        actions: <Widget> [
+          IconButton(icon: Icon(Icons.refresh),
+            onPressed: () {}, )
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: const <Widget>[
+          Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+          TextField(keyboardType: TextInputType.number,
+          decoration:  InputDecoration(
+            labelText: "Peso (kg)",
+            labelStyle: TextStyle(fontSize: 15.0, color: Colors.green)
+          ),
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.green,
+          ),
+          ),
+          TextField(keyboardType: TextInputType.number,
+          decoration:  InputDecoration(
+            labelText: "Altura (CM)",
+            labelStyle: TextStyle(fontSize: 15.0, color: Colors.green)
+          ),
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.green,
+          ),
+          )
+        ],
+      ),
+    );
+  }
+}
